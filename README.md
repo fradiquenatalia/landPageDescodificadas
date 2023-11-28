6a9d1531b9cbb67bdb6e3a8189ba1a831b5bc4f7
fradiquenatalia <fradiquenatalia@gmail.com>
2023-11-28 16:32:26 -0300

    Create Grid display on Recipes Page

---
+++ README.md
@@ -0,0 +1,1 @@
+Landpage criada para o curso Descodificadas Trilha 2 - 2023.2, usando HTML, CSS e JavaScript.
--- script.js
+++ script.js
@@ -1,46 +1,37 @@
-function exibirValores(nome, email) {
-    var valoresDiv = document.getElementById("valores");
-    valoresDiv.innerHTML = "<p><strong>Nome:</strong> " + nome + "</p>" +
-      "<p><strong>Email:</strong> " + email + "</p>";
-  }
-  
   function enviarFormulario() {
-    var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
   
-    if (nome === "") {
+    if (!nome) {
       alert("Por favor, preencha o campo Nome.");
       return false;
     }
   
-    if (email === "") {
+    if (!email) {
       alert("Por favor, preencha o campo Email.");
       return false;
     }
-    return exibirValores(nome, email);
+     exibirValores(nome, email);
   }
   
-  var formularioValores = [];
+  let formularioValores = [];
   
-  function exibirValores() {
-    var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
-  
+  function exibirValores(nome,email) {
     formularioValores.push({ nome: nome, email: email });
   
-    var valoresDiv = document.getElementById("valoresEnviados");
+    let valoresDiv = document.getElementById("valoresEnviados");
     valoresDiv.innerHTML = "";
-    for (var i = 0; i < formularioValores.length; i++) {
+    for (let i = 0; i < formularioValores.length; i++) {
       valoresDiv.innerHTML += "<p><strong>Nome:</strong> " + formularioValores[i].nome + "</p>" +
         "<p><strong>Email:</strong> " + formularioValores[i].email + "</p>";
     }
   }
   
   function initMap() {
-    var latitude = -8.0475626;
-    var longitude = -34.8770091;
-    var mapElement = document.getElementById("map");
-    var iframe = document.createElement("iframe");
+    let latitude = -8.0475626;
+    let longitude = -34.8770091;
+    let mapElement = document.getElementById("map");
+    let iframe = document.createElement("iframe");
     iframe.src = "https://www.openstreetmap.org/export/embed.html?bbox=" +
       (longitude - 0.01) + "," + (latitude - 0.01) + "," +
       (longitude + 0.01) + "," + (latitude + 0.01) +
